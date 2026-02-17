b487b0f9dedb38d31b4da561ac025816accdb3be
Ademir Souza de Almeida <centroserra@gmail.com>
2026-02-17 14:16:54 -0300

    Deploy da aplicação

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve arquivos estáticos da build
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Todas as rotas vão para index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
