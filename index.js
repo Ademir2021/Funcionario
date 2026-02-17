e2592cf3329fc18cee8c349af433c18405271854
Ademir Souza de Almeida <centroserra@gmail.com>
2026-02-17 14:08:28 -0300

    Deploy da aplicação

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 const PORT = process.env.PORT || 3000
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build'));
 });
 
 app.listen(PORT, () => {
