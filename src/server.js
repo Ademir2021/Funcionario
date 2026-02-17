91aa18d7175dbac8ee4f2d672226b2b716fec975
Ademir Souza de Almeida <centroserra@gmail.com>
2026-02-17 15:02:04 -0300

    Deploy da aplicação

--- src/server.js
+++ src/server.js
@@ -3,16 +3,16 @@ const path = require('path');
 const app = express();
 
 // Serve arquivos estáticos da build
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build', 'index.html')));
 
 // Todas as rotas vão para index.html
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-
 app.get('/', (req, res) => {
-  res.send('Olá! Seu servidor Node e Express está funcionando.');
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+// app.get('/', (req, res) => {
+//   res.send('Olá! Seu servidor Node e Express está funcionando.');
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
